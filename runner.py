ef57e02f529d7bcf24d5598d405dbb4e78ac4052
Ahmed Yakub <ahmedyakub@Ahmeds-MacBook-Pro.local>
2024-12-03 20:23:49 +0100

    commit2

---
+++ runner.py
@@ -0,0 +1,7 @@
+from UserInputValidator import UserInputValidator
+
+input_data1 = ["abc","789","-95", "0.11", "123", "456"]
+validatorObject1 = UserInputValidator()
+
+input_data2 = ["0.002","-789","95", "11", "123", "xyz"]
+validatorObject2 = UserInputValidator()
